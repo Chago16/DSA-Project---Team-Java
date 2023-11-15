d95f1897ae38bdd0b8745c56352babc9aa645aaa
James Esteban <james.esteban08@gmail.com>
2023-11-16 03:08:11 +0800

    Fourth Com - Added JDialogue

---
+++ Algo.java
@@ -0,0 +1,8 @@
+public class Algo {
+    
+    int initBudget = 0;
+    int overAllIncome = 0;
+    int overAllExpenses = 0;
+    int curBudget = 0;
+    int tempholder = 0;
+}
--- HomePage.java
+++ HomePage.java
@@ -1,3 +1,7 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
+
 import javax.swing.*;
 
 
@@ -8,10 +12,50 @@ public class HomePage {
 
     public HomePage() {
 
+        Algo sharedVar = new Algo();
+        sharedVar.initBudget = 0;
+
         //make the home page here
         homePanel = new JPanel();
         JLabel hi = new JLabel("HI This is Home Panel");
+
+        //button for adding init budget
+
+        JButton add = new JButton("Declare Initial Budget");
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JDialog addDialog = new JDialog();
+                addDialog.setTitle("Add an Account");
+                addDialog.setSize(250, 120);
+                addDialog.setLocationRelativeTo(null);
+                addDialog.setLayout(new FlowLayout());
+
+
+
+                JTextField userinp = new JTextField(15);
+                addDialog.add(userinp);
+
+                JButton subButton = new JButton("Confirm");
+                addDialog.add(subButton);
+
+                JButton canButton = new JButton("Cancel");
+                addDialog.add(canButton);
+
+                addDialog.setVisible(true);
+            }
+        });
+        
+
+
+
+
+        homePanel.add(add);
         homePanel.add(hi);
+
+        
+
+
     //for MENU
     }
     
