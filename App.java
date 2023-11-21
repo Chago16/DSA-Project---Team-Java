2bf7e10655b11bb8c6ced08c68bc13b5f0b4259e
James Esteban <james.esteban08@gmail.com>
2023-11-21 17:03:05 +0800

    Removed all files

--- App.java
+++
@@ -1,8 +0,0 @@
-import java.sql.SQLException;
-
-public class App {
-    public static void main(String[] args) throws SQLException {
-        login login = new login();
-        login.loginView();
-    }
-}
