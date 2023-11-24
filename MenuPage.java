3428d111607dc5aaaef4f7107d06439836780927
jennarms <148486734+jennarms@users.noreply.github.com>
2023-11-24 11:42:44 +0800

    bigger panel, bigger wider buttons, buttons with icons

--- MenuPage.java
+++ MenuPage.java
@@ -1,7 +1,15 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
 
-
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MenuPage {
 
@@ -12,56 +20,62 @@ public class MenuPage {
     private JButton expensesButton;
     private JButton goalButton;
 
-    //public instance of the menu frame for calling it on the main file
+    // public instance of the menu frame for calling it on the main file
     public JPanel menuPanel;
 
-
     public MenuPage() {
-    //make the menu setup here
+        // make the menu setup here
+        menuPanel = new JPanel(); // menu panel instance
+        menuPanel.setBackground(Color.decode("#2c2a2a"));
+        menuPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(20, 20, 5, 20); // Adjusted left margin
 
-    
-    menuPanel = new JPanel(); //menu panel instance
-    menuPanel.setBackground(Color.decode("#2c2a2a"));
-    menuPanel.setLayout(new GridBagLayout());
-    GridBagConstraints gbc = new GridBagConstraints();
-    gbc.insets = new Insets(20, 20, 5, 20);
+        // Add image above buttons
+        ImageIcon logoIcon = new ImageIcon("LogoMenuPage.png");
+        JLabel logoLabel = new JLabel();
+        logoLabel.setIcon(new ImageIcon(logoIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
+        gbc.gridy = 0;
+        menuPanel.add(logoLabel, gbc);
 
-    //styling the buttons 
-    
-    homeButton = new JButton("Home");
-    homeButton.setBackground(Color.decode("#FFFFFF"));
-    homeButton.setForeground(Color.decode("#FF914D"));
-    homeButton.setPreferredSize(new Dimension(100, 40));
 
-    incomeButton = new JButton("Income");
-    incomeButton.setBackground(Color.decode("#FFFFFF"));
-    incomeButton.setForeground(Color.decode("#FF914D"));
-    incomeButton.setPreferredSize(new Dimension(100, 40));
+        // styling the buttons
+        homeButton = createButton("Home", "home.png");
+        incomeButton = createButton("Income", "income.png"); // Replace "income.png" with the actual icon file name
+        expensesButton = createButton("Expenses", "expenses.png"); // Replace "expenses.png" with the actual icon file name
+        goalButton = createButton("Goal", "goal.png"); // Replace "goal.png" with the actual icon file name
 
-    expensesButton = new JButton("Expenses");
-    expensesButton.setBackground(Color.decode("#FFFFFF"));
-    expensesButton.setForeground(Color.decode("#FF914D"));
-    expensesButton.setPreferredSize(new Dimension(100, 40));
-
-    goalButton = new JButton("Goal");
-    goalButton.setBackground(Color.decode("#FFFFFF"));
-    goalButton.setForeground(Color.decode("#FF914D"));
-    goalButton.setPreferredSize(new Dimension(100, 40));
-
-    //styling buttons end ====
-
-    gbc.gridy = 0;
-    menuPanel.add(homeButton, gbc);
-    gbc.gridy = 1;
-    menuPanel.add(incomeButton, gbc);
-    gbc.gridy = 2;
-    menuPanel.add(expensesButton, gbc);
-    gbc.gridy = 3;
-    menuPanel.add(goalButton, gbc);
-
+        // Add buttons to the menu panel
+        gbc.gridy = 2;
+        menuPanel.add(homeButton, gbc);
+        gbc.gridy = 3;
+        menuPanel.add(incomeButton, gbc);
+        gbc.gridy = 4;
+        menuPanel.add(expensesButton, gbc);
+        gbc.gridy = 5;
+        menuPanel.add(goalButton, gbc);
     }
 
-    //card layout instance on menu
+    private JButton createButton(String buttonText, String iconName) {
+        JButton button = new JButton(buttonText);
+
+        // Load the icon from the file or resource
+        try {
+            ImageIcon icon = new ImageIcon(iconName);
+            Image image = icon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+            button.setIcon(new ImageIcon(image));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        button.setBackground(Color.decode("#FFFFFF"));
+        button.setForeground(Color.decode("#FF914D"));
+        button.setPreferredSize(new Dimension(150, 40)); // Adjust the width value here
+
+        return button;
+    }
+
+    // card layout instance on menu
     public void setCardLayout(CardLayout cardLayout, JPanel mainPanel) {
         this.cardLayout = cardLayout;
         this.mainPanel = mainPanel;
@@ -76,7 +90,4 @@ public class MenuPage {
         goalButton.addActionListener(e -> cardLayout.show(mainPanel, "GoalPage"));
         // Add action listeners for other buttons as needed
     }
-
-
-
 }
