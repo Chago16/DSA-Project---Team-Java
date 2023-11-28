62c34d86800227a2e3e3a3b6335909a0dfe0b1df
jennarms <148486734+jennarms@users.noreply.github.com>
2023-11-29 01:47:29 +0800

    minor update

--- ExpensePage.java
+++ ExpensePage.java
@@ -5,8 +5,6 @@ import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -185,14 +183,6 @@ public class ExpensePage {
                     textAmount.setText("");
                     textLabel.setText("");
                 }
-            }
-        });
-
-        // get selected row data From table to textfields
-        expTable.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                // Add your logic here if needed
             }
         });
 
